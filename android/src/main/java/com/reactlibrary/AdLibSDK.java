139ab932a561c5d14d84744c4331c14d3cecc522
Ozan Karaali <ozankaraali@users.noreply.github.com>
2020-04-23 18:43:28 +0300

    AdLibSDK GDPR

--- android/src/main/java/com/reactlibrary/AdLibSDK.java
+++ android/src/main/java/com/reactlibrary/AdLibSDK.java
@@ -7,15 +7,22 @@ import android.os.Handler;
 import com.mopub.common.MoPub;
 import com.mopub.common.SdkConfiguration;
 import com.mopub.common.SdkInitializationListener;
+import com.mopub.common.privacy.ConsentDialogListener;
+import com.mopub.common.privacy.PersonalInfoManager;
+import com.mopub.mobileads.MoPubErrorCode;
 import com.mopub.common.logging.MoPubLog;
+import androidx.annotation.NonNull;
 
 /**
  * Created by usamaazam on 29/03/2019.
  */
 
 public class AdLibSDK {
-
     static void initializeAdSDK(final RNMoPubBanner banner, final String adUnitId, final Activity context) {
+        initializeAdSDKWithGDPR(banner, adUnitId, context, false);
+    }
+
+    static void initializeAdSDKWithGDPR(final RNMoPubBanner banner, final String adUnitId, final Activity context, final Boolean shouldShowGDPR) {
 
         Handler mainHandler = new Handler(context.getMainLooper());
 
@@ -36,11 +43,36 @@ public class AdLibSDK {
                 return new SdkInitializationListener() {
                     @Override
                     public void onInitializationFinished() {
+                        PersonalInfoManager mPersonalInfoManager = MoPub.getPersonalInformationManager();
+                        if (mPersonalInfoManager != null && mPersonalInfoManager.shouldShowConsentDialog()) {
+                            mPersonalInfoManager.loadConsentDialog(initDialogLoadListener());
+                        }
+                        
                         if (banner != null) {
                             banner.setAdUnitId(adUnitId);
                             banner.loadAd();
                         }
 
+                    }
+                };
+            }
+
+            private ConsentDialogListener initDialogLoadListener() {
+                return new ConsentDialogListener() {
+        
+                    @Override
+                    public void onConsentDialogLoaded() {
+                        PersonalInfoManager mPersonalInfoManager = MoPub.getPersonalInformationManager();
+                        if (mPersonalInfoManager != null) {
+                            mPersonalInfoManager.showConsentDialog();
+                            //mShowingConsentDialog = true;
+                        }
+                    }
+        
+                    @Override
+                    public void onConsentDialogLoadFailed(@NonNull MoPubErrorCode moPubErrorCode) {
+                        MoPubLog.i("Consent dialog failed to load.");
+                        // Utils.logToast(MoPubSampleActivity.this, "Consent dialog failed to load.");
                     }
                 };
             }
