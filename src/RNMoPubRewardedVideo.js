dd25a5dd40906ee0baeb0eb77568abc6b5e70961
Ozan Karaali <ozankaraali@users.noreply.github.com>
2020-04-23 20:11:11 +0300

    WIP GDPR Video

--- src/RNMoPubRewardedVideo.js
+++ src/RNMoPubRewardedVideo.js
@@ -5,8 +5,9 @@ const { RNMoPubRewardedVideo } = NativeModules;
 const emitter = new NativeEventEmitter(RNMoPubRewardedVideo);
 
 module.exports = {
-    initializeSdkForRewardedVideoAd: (adUnitId:string) => RNMoPubRewardedVideo.initializeSdkForRewardedVideoAd(adUnitId),
+    initializeSdkForRewardedVideoAd: (adUnitId:string, shouldShowGDPR: bool) => RNMoPubRewardedVideo.initializeSdkForRewardedVideoAd(adUnitId, shouldShowGDPR),
     loadRewardedVideoAdWithAdUnitID: (adUnitId: string) => RNMoPubRewardedVideo.loadRewardedVideoAdWithAdUnitID(adUnitId),
+    showRewardedVideoAdForAdUnitID: (adUnitId: string) => RNMoPubRewardedVideo.showRewardedVideoAdForAdUnitID(adUnitId),
     presentRewardedVideoAdForAdUnitID: (adUnitId: string, currencyType:string, amount: number, promise:()=>{}) => RNMoPubRewardedVideo.presentRewardedVideoAdForAdUnitID(adUnitId, currencyType, amount, promise),
     availableRewardsForAdUnitID: (adUnitId:string, promise:()=>{}) => RNMoPubRewardedVideo.availableRewardsForAdUnitID(adUnitId,promise),
     hasAdAvailableForAdUnitID:(adUnitId:string, promise:()=>{}) =>RNMoPubRewardedVideo.hasAdAvailableForAdUnitID(adUnitId,promise) ,
